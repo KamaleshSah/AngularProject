69b651070232282d42bd2749685123363e3e5c39
KamaleshSah <kamalesh.sah07@gmail.com>
2022-08-05 02:36:20 +0530

    Header Footer Component

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,12 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserReportComponent } from './user-report/user-report.component';
 import { ExamReportComponent } from './exam-report/exam-report.component';
+import { AdminComponent } from './admin.component';
 
 
 const routes: Routes = [
-  {path:"",component:HomeComponent},
-  {path:"user-report",component:UserReportComponent},
-  {path:"exam-report",component:ExamReportComponent}
+  {path:"",
+  component:AdminComponent,
+  children:[
+    {path:"",component:HomeComponent},
+    {path:"user-report",component:UserReportComponent},
+    {path:"exam-report",component:ExamReportComponent},
+  ]
+}
+ 
 ];
 
 @NgModule({
